d0a419b492c7be16418c0997493e25f1bef27fe4
Valentin Kolev <vallentin.k@gmail.com>
2019-05-16 00:19:27 +0300

    converted page ti xaml page

--- Phoneword/MainPage.xaml.cs
+++ Phoneword/MainPage.xaml.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using Xamarin.Essentials;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -12,9 +12,51 @@ namespace Phoneword
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class MainPage : ContentPage
     {
+        string translatedNumber;
+
         public MainPage()
         {
             InitializeComponent();
+        }
+
+
+        private void OnTranslate(object sender, EventArgs e)
+        {
+            translatedNumber = phoneNumberText.Text.ToNumber();
+
+            if (!string.IsNullOrEmpty(translatedNumber))
+            {
+                callButton.IsEnabled = true;
+                callButton.Text = $"Call {translatedNumber}";
+            }
+            else
+            {
+                callButton.IsEnabled = false;
+                callButton.Text = "Call";
+            }
+        }
+
+        private async void OnCall(object sender, EventArgs e)
+        {
+            if (await DisplayAlert("Dial a Number", $"Would you like to call {translatedNumber}?", "Yes", "No"))
+            {
+                try
+                {
+                    PhoneDialer.Open(translatedNumber);
+                }
+                catch (ArgumentNullException)
+                {
+                    await DisplayAlert("Unable to dial", "Phone number was not valid.", "OK");
+                }
+                catch (FeatureNotSupportedException)
+                {
+                    await DisplayAlert("Unable to dial", "Phone dialing not supported.", "OK");
+                }
+                catch (Exception)
+                {
+                    await DisplayAlert("Unable to dial", "Phone dialing failed.", "OK");
+                }
+            }
         }
     }
 }
---
+++ Phoneword/SharedResources.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace Phoneword
+{
+    public static class SharedResources
+    {
+        public static Color ButtonBackgroundColor => Color.FromRgb(0xff, 0xa5, 0);
+
+        public static Color ButtonTextColor => Color.White;
+    }
+}
